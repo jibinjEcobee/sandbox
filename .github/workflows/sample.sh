d71a4b95a7a76567f2d80aab882ad0ec60937438
Jibin Jacob <jibinj@ecobee.com>
2020-02-17 22:12:23 -0500

    almost there

--- .github/workflows/sample.sh
+++ .github/workflows/sample.sh
@@ -25,5 +25,5 @@ count=$(($count + 1))
 echo $commit $delta mins ";" $avg
 done
 
-echo "avg lead time for $latestTag =" $(floatCalculator $avg/$count) "mins"
-echo "avg lead time for $latestTag =" $(floatCalculator $avg/$count) "mins" > output.txt
+echo "avg lead time for $1 =" $(floatCalculator $avg/$count) "mins"
+echo "avg lead time for $1 =" $(floatCalculator $avg/$count) "mins" > output.txt
